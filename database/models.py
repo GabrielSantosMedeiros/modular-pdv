b88d824549c16bac7bd07900d82a83b44a3c35b4
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2025-04-17 18:29:07 -0300

    create pagamento model

--- database/models.py
+++ database/models.py
@@ -24,11 +24,12 @@ class Produto(models.Model):
 
 class Carrinho(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    cliente = models.CharField(max_length=255)
-    contato = models.TextField()
-    endereco = models.TextField()
+    cliente = models.CharField(max_length=255, null=True)
+    contato = models.TextField(null=True)
+    endereco = models.TextField(null=True)
     status_entrega = models.CharField(default='PENDENTE')
     status_pagamento = models.CharField(default='PENDENTE')
+    total = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(default=datetime.now)
     updated_at = models.DateTimeField(default=datetime.now)
 
@@ -49,3 +50,18 @@ class ItemCarrinho(models.Model):
     class Meta:
         database = provider.getDatabase()
         table_name = 'item_carrinho'
+
+
+
+class Pagamento(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    carrinho = models.ForeignKeyField(model=Carrinho, backref='pagamentos')
+    tipo_pagamento = models.CharField(max_length=20)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem_comprovante = models.TextField(null=True)
+    created_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = provider.getDatabase()
+        table_name = 'pagamentos'
