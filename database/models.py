919ead3eedad9315783a107d913d3a7d11f29394
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2025-04-17 18:12:45 -0300

    create item_carrinho model

--- database/models.py
+++ database/models.py
@@ -12,9 +12,25 @@ class Produto(models.Model):
     descricao = models.TextField()
     imagem = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=datetime.now)
     updated_at = models.DateTimeField(default=datetime.now)
 
     class Meta:
         database = provider.getDatabase()
         table_name = 'produtos'
+
+    
+
+class ItemCarrinho(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    produto = models.ForeignKeyField(model=Produto)
+    quantidade = models.IntegerField(default=1)
+    created_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = provider.getDatabase()
+        table_name = 'item_carrinho'
+
+
